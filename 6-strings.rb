f6f1eb5675cbef1ca5dcdb8f4804d847aa66275a
rafabuilds <rafaelcontreira.dev@hotmail.com>
2025-09-18 23:35:54 -0300

    String class

--- 6-strings.rb
+++ 6-strings.rb
@@ -1,1 +1,17 @@
 #Movie Data
+name = "Back to the Future"
+name2 = "back to the future"
+
+#Ruby is case-sensitive.
+puts name == name2
+
+description = "The film is about time travel."
+description2 = <<Text
+  The film #{name} is a 1995 classic about time travel.
+Text
+
+puts description2
+
+#String size
+puts description.length
+puts description.size
